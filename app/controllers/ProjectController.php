b1ac8178e732daf9dc8cff1c1ee95a68a7003ef4
guodong <gd@tongjo.com>
2015-01-21 21:00:13 +0800

    du

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -14,6 +14,7 @@ class ProjectController extends BaseController {
 	    $project = Project::find($id);
 	    $project->categorys;
 	    $project->creator;
+	    $project->comments;
 	    
 	    return $project->toJson();
 	}
