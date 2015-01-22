d0d32088f37b389a9b0e2b78143c3080d15e7036
yangy0504 <yangy0504@126.com>
2015-01-22 19:57:37 +0800

    123

---
+++ app/controllers/CommentListController.php
@@ -0,0 +1,47 @@
+<?php
+use Illuminate\Support\Facades\Response as Responses;
+use Illuminate\Support\Facades\Input;
+
+class CommentListController extends BaseController {
+
+	public function index()
+	{
+		$project = Project::whereRaw('id = ?', array(Input::get('projectId')))->first();
+		if ($project)
+		{
+			$comments = $project->comments;
+			$count = count($comments);
+			if ($count != 0)
+			{	
+				for ($i = 0 ; $i <= $count-1; $i++)
+				{
+					$CommentList[$i] = array( 
+						'commentUserId' => $comments[$i]->user_id,
+						'commentUserName'=> User::find($comments[$i]->user_id)->realname,
+						'commentUserImage' => User::find($comments[$i]->user_id)->avatar,
+						'commentText' => $comments[$i]->content,
+						'commentDate' => date($comments[$i]->created_at));
+				}
+			
+				$response = array( 
+					'result' => array(
+						'code' =>0, 'message' => 'no problem'), 
+					'count' => $count,
+					'commentList' => $CommentList);
+				return Responses::json($response);
+			}
+			else 
+			{
+				return Responses::json(array(
+					'result' => array(
+						'code' =>0, 'message' => 'no problem'),
+					'count' => 0));
+			}
+		}
+		else
+		{
+			return Responses::json(array('result' => array('code' =>1, 'message' => 'problem 1')));
+		}
+	}
+	
+}
--- app/routes.php
+++ app/routes.php
@@ -27,6 +27,7 @@ Route::resource('team.user', 'TeamUserController');
 Route::resource('category', 'CategoryController');
 Route::resource('team', 'TeamController');
 Route::resource('teamlist', 'TeamListController');
+Route::resource('commentlist', 'CommentListController');
 Route::resource('school', 'SchoolController');
 Route::resource('major', 'MajorController');
 Route::resource('team.user.status', 'TeamUserStatusController');
