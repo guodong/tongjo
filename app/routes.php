10ffd54825fd2e71a00ac14cb3d83f35a8828f33
yangy0504 <yangy0504@126.com>
2015-01-25 16:10:15 +0800

    123

--- app/routes.php
+++ app/routes.php
@@ -35,6 +35,7 @@ Route::resource('user.avatar', 'UserAvatarController');
 Route::resource('projectdetail', 'ProjectDetailController');
 Route::resource('projectlist', 'ProjectListController');
 Route::resource('login', 'LoginController');
+Route::resource('userdetail', 'UserDetailController');
 Route::resource('project.team', 'ProjectTeamController'); //获取参加某项目的团队
 Route::resource('tag', 'TagController');
 Route::resource('comment', 'CommentController');
