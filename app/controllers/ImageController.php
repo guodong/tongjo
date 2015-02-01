4b8aef9a953ea3ff8c935e6ce3ecf2bb74069bf0
guodong <gd@tongjo.com>
2015-02-01 16:53:14 +0800

    update image upload as meituxiuxiu, add session start

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -5,6 +5,26 @@ class ImageController extends BaseController {
 	
 	public function store()
 	{
+	    $post_input = 'php://input';
+	    $save_path = PATH_BASE.'public/files/';
+	    $postdata = file_get_contents( $post_input );
+	    
+	    if ( isset( $postdata ) && strlen( $postdata ) > 0 ) {
+	        $fn = uniqid().'jpg';
+	        $filename = $save_path . $fn;
+	        $handle = fopen( $filename, 'w+' );
+	        fwrite( $handle, $postdata );
+	        fclose( $handle );
+	        if ( is_file( $filename ) ) {
+	            return json_encode(array('file'=>$fn));
+	            exit ();
+	        }else {
+	            die ( 'Image upload error!' );
+	        }
+	    }else {
+	        die ( 'Image data not detected!' );
+	    }
+	    return;
 	    $fn = time().'.jpg';
 	    $dst = PATH_BASE.'public/files/'.$fn;
 	    $img = str_replace('data:image/png;base64,', '', Input::get('image'));
