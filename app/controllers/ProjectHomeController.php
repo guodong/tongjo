1254cdbb4b76fbdf5534df35f880fb059a175339
yangy0504 <yangy0504@126.com>
2015-02-04 15:59:47 +0800

    123

--- app/controllers/ProjectHomeController.php
+++ app/controllers/ProjectHomeController.php
@@ -7,6 +7,7 @@ class ProjectHomeController extends BaseController {
 	public function index()
 	{
 		$user = User::whereRaw('id = ?', array(Input::get('userId')))->first();
+		$category = Category::whereRaw('id = ?', array(Input::get('categoryId')))->first();
 		$categoryId =  (int) Input::get('categoryId');
 		$customId = (int) Input::get("customId");
 		
@@ -18,8 +19,6 @@ class ProjectHomeController extends BaseController {
 			
 			if ($categoryId == 0)
 			{
-				
-				
 				if ($customId == 1)
 				{
 					for ($i = 0 ; $i <= 2; $i++)
@@ -128,19 +127,50 @@ class ProjectHomeController extends BaseController {
 				{
 					$projectList = NULL;
 				}
-			}					
+			}
+			
+			else if ($categoryId != 0 && $categoryId != NULL)
+			{
+				$projects = $category->projects;
+				$count = count($projects);
+				for ($i = 0 ; $i <= $count-1; $i++)
+				{
+					$projectCreator = $projects[$i]->creator;
+					$projectList[$i] = array( 'projectID' => $projects[$i]->id,
+							'projectName' => $projects[$i]->name,
+							'projectImage' => $projects[$i]->image,
+							'projectCreatedDate' => date($projects[$i]->created_at),
+							'projectEndDate' => $projects[$i]->deadline,
+							'projectFounderId' => $projects[$i]->user_id,
+							'projectFounderName' => $projects[$i]->creator->realname,
+							'projectFounderImage' => $projects[$i]->creator->avatar,
+							'projectFounderUniversityId' => $projects[$i]->creator->school_id,
+							'projectFounderUniversityName' => School::find($projects[$i]->creator->school_id)->name,
+							'projectLabel' => $projects[$i]->categorys->first()->name,
+							'projectText' => $projects[$i]->description,
+							'teamNumber' => count($projects[$i]->teams),
+							'commentNumber' => count($projects[$i]->comments));
+				}
+			}
+			
+			else {
+				$projectList = NULL;
+			}
+			
 			if ($projectList){
 				$response = array( 'result' => array('code' =>0, 'message' => 'no problem'), 
 								   'categoryId' => $categoryId,
 								   'customId' => $customId,
 							       'adProjects' => $adProjects,
-							       'projectList' => $projectList,
-	                          	 );
+							       'projectList' => $projectList);
 				return Responses::json($response);
 			}
 			else{
 				return Responses::json(array('result' => array('code' =>1, 'message' => 'problem 1')));
 			}
+		}
+		else {
+			return Responses::json(array('result' => array('code' =>1, 'message' => 'problem 1')));
 		}
 	}
 	
