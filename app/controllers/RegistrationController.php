ed8fbc923a6f3ca81654ed244a8644ab048a76eb
yangy0504 <yangy0504@126.com>
2015-04-01 10:16:38 +0800

    123

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -16,7 +16,7 @@ class RegistrationController extends BaseController {
 			//if (isset($user->email) && isset($user->password))
 			//{
 				//$user->hxid = "hx_".(string)$user->id;
-				$url = 'https://a1.easemob.com/easemob-demo/chatdemoui/token';
+				/*$url = 'https://a1.easemob.com/easemob-demo/chatdemoui/token';
 				$get_token = array();
 				$get_token['grant_type'] = 'client_credentials';
 				$get_token['client_id'] = 'YXA6VP5zQMolEeS6LregkdHd4g';
@@ -29,8 +29,63 @@ class RegistrationController extends BaseController {
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $get_token);
 				$output = curl_exec($ch);
 				curl_close($ch);
-				return $output;
-				
+				return $output;*/
+			$formgettoken="https://a1.easemob.com/easemob-demo/chatdemoui/token";
+			$body=array(
+					"grant_type"=>"client_credentials",
+					"client_id"=>"YXA6VP5zQMolEeS6LregkdHd4g",
+					"client_secret"=>"YXA6OuKmp8eotsVaUYdbcLRqYCIkbik"
+			);
+			$patoken=json_encode($body);
+			$res = _curl_request($formgettoken,$patoken);
+			$tokenResult = array();
+			
+			$tokenResult =  json_decode($res, true);
+			print_r($tokenResult);
+			echo "/n";
+			
+			function _curl_request($url, $body, $header = array(), $method = "POST")
+			{
+				array_push($header, 'Accept:application/json');
+				array_push($header, 'Content-Type:application/json');
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		
+				switch ($method){
+					case "GET" :
+						curl_setopt($ch, CURLOPT_HTTPGET, true);
+						break;
+					case "POST":
+						curl_setopt($ch, CURLOPT_POST,true);
+						break;
+					case "PUT" :
+						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+						break;
+					case "DELETE":
+						curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+						break;
+				}
+			
+				curl_setopt($ch, CURLOPT_USERAGENT, 'SSTS Browser/1.0');
+				curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+				if (isset($body{3}) > 0) {
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+				}
+				if (count($header) > 0) {
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+				}
+				$ret = curl_exec($ch);
+				$err = curl_error($ch);
+				curl_close($ch);
+				if ($err) {
+					return $err;
+						
+				}
+				return $ret;
+			}
+						
 				
 				//if (isset($user->hxid))
 				//return Responses::json(array(
